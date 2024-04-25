2f5bbb20b05aa63d15f4334c5fa6e172ccc1ed2f
DaShaun <dashaun@dashaun.com>
2024-04-24 20:00:18 -0500

    3.2.5, builder, yaml

--- src/main/java/dev/dashaun/ai/ollama/llamatwochat/ChatController.java
+++ src/main/java/dev/dashaun/ai/ollama/llamatwochat/ChatController.java
@@ -4,7 +4,6 @@ import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +16,17 @@ class ChatController {
 
     private final OllamaChatClient chatClient;
 
-    @Autowired
     ChatController(OllamaChatClient chatClient) {
         this.chatClient = chatClient;
     }
 
     @GetMapping("/llama2/generate")
-    Map generate(@RequestParam(value = "message", defaultValue = "Tell me how to say thanks for attending my session and its time for questions") String message) {
+    Map generate(@RequestParam(defaultValue = "Tell me how to say thanks for attending my session and its time for questions") String message) {
         return Map.of("generation", chatClient.call(message));
     }
 
     @GetMapping("/llama2/generateStream")
-    Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+    Flux<ChatResponse> generateStream(@RequestParam(defaultValue = "Tell me a story about Missouri") String message) {
         Prompt prompt = new Prompt(new UserMessage(message));
         return chatClient.stream(prompt);
     }
